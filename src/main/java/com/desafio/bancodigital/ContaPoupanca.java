c4953bcdbed88164d116f1e63ea4bb021cec3d98
Maiquera <maicolmenezes@gmail.com>
2022-03-22 10:29:31 -0300

    heraca

--- src/main/java/com/desafio/bancodigital/ContaPoupanca.java
+++ src/main/java/com/desafio/bancodigital/ContaPoupanca.java
@@ -1,23 +1,6 @@
 package com.desafio.bancodigital;
 
-public class ContaPoupanca {
-
-    private int agencia;
-    private int numero;
-    private double saldo;
-
-
-    public void sacar(){
-
-    }
-
-    public void depositar(){
-
-    }
-
-    public void transferir(){
-
-    }
+public class ContaPoupanca extends Conta {
 
 
 }
